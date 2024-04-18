59380aca96b1f8a107ab6d7b742859c0b72c0d0e
Carson Wolff <160627306+carsonwolff@users.noreply.github.com>
2024-04-18 14:37:01 -0400

    Add files via upload

---
+++ PrimeFactorizer.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+/**
+ * @author Carson Wolff
+ * @version 1.0
+ */
+public class PrimeFactorizer {
+    private long num;
+    private ArrayList<Long> factors;
+    public PrimeFactorizer(long n){
+        this.num = n;
+        this.factors = new ArrayList<>(1);
+        factors.add(1L);
+    }
+    public void factorize(long n, ArrayList<Long> primesTo){
+        System.out.println("Finding prime factors of " + n);
+        if(n == 1){
+            return;
+        }
+        for (long prime: primesTo) {
+            if(prime > Math.sqrt(n)){
+                factors.add(n);
+                break;
+            }
+            if(n % prime == 0){
+                System.out.println(prime);
+                factors.add(prime);
+                n /= prime;
+                factorize(n,primesTo);
+                break;
+            }
+        }
+    }
+    public static long getNumFromUser(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a number to find primes factors for: ");
+        long n = in.nextLong();
+        if (n < 2) {
+            System.out.println("Please enter a number greater than 1");
+            n = getNumFromUser();
+        }
+        return n;
+    }
+    public void printPrimes(){
+        System.out.println("Printing prime factors of " + num);
+        factors.add(num);
+        for(long prime: factors){
+            System.out.printf("%d ", prime);
+        }
+    }
+    public static void main(String[] args) {
+        long n = getNumFromUser();
+        long m = (long) Math.sqrt(n);
+        PrimeFactorizer factor = new PrimeFactorizer(n);
+        PrimeGenerator generate = new PrimeGenerator(m);
+        generate.generate();
+        ArrayList<Long> primesToSqrtN = generate.getPrimes();
+        factor.factorize(n,primesToSqrtN);
+        factor.printPrimes();
+    }
+}
---
+++ PrimeGenerator.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author Carson Wolff
+ * @version 1.0
+ * Thank you for checking out my code!
+ */
+public class PrimeGenerator {
+    private long num;
+    private ArrayList<Long> primes;
+    public PrimeGenerator(long n){
+        this.num = n;
+        this.primes = new ArrayList<>();
+        primes.add(2L);
+        primes.add(3L);
+    }
+    public void generate(){
+        System.out.println("Finding primes up to " + num);
+        System.out.println(2);
+        System.out.println(3);
+        for (long i = 5; i <= num; i+=2) {
+            boolean isPrime = true;
+            for (Long prime : primes) {
+                if (i % prime == 0) {
+                    isPrime = false;
+                    break;
+                }
+                if (prime >Math.sqrt(i)) {
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.println(i);
+                primes.add(i);
+            }
+        }
+    }
+    public static int getNumFromUser(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a number to find primes to: ");
+        int n = in.nextInt();
+        if (n < 4) {
+            System.out.println("Please enter a number greater than 3");
+            n = getNumFromUser();
+        }
+        return n;
+    }
+    public ArrayList<Long> getPrimes() {
+        return primes;
+    }
+    public static void main(String[] args){
+        int n = getNumFromUser();
+        PrimeGenerator generator = new PrimeGenerator(n);
+        generator.generate();
+    }
+}
